e69b2e6dec7f4808079372e4d482f98796a0edca
Victor-maestro <victor_maestro@mail.ru>
2021-01-02 22:38:04 +0300

    edit menu & add scroll script

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,36 @@
 $(function() {
 	
 	// Mask form
-	$('input[name="Lead[phone]"]').mask('8 (999) 999 - 99 - 99');
+	$('input[name="phone"]').mask('9 (999) 999 - 99 - 99');
 
 
+	// Scroll menu
+  var width = $(this).width();
+
+  $(window).innerWidth(function () {
+
+    if (width > 768) {
+
+      $(this).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+          $(".menu").addClass("menu_fixed");
+        } else if ($(this).scrollTop() < 200) {
+          $(".menu").removeClass("menu_fixed");
+        }
+      });
+
+    } else {
+      $(".menu").addClass("menu_hidden");
+    }
+	});
+	
+	// Open & close menu
+  $(".toggle-menu").click(function () {
+    $(this).toggleClass("toggle-menu_open");
+    $(this).siblings("header").find(".menu").toggleClass("menu_fixed menu_hidden");
+    return false;
+	});
+	
 });
 
 /* Javascript */
