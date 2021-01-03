156813d359051b7df62824a038ec424edc45c884
Victor-maestro <victor_maestro@mail.ru>
2021-01-03 18:57:53 +0300

    section resume

--- app/js/main.js
+++ app/js/main.js
@@ -24,14 +24,35 @@ $(function() {
       $(".menu").addClass("menu_hidden");
     }
 	});
-	
+  
+  
 	// Open & close menu
   $(".toggle-menu").click(function () {
     $(this).toggleClass("toggle-menu_open");
     $(this).siblings("header").find(".menu").toggleClass("menu_fixed menu_hidden");
     return false;
-	});
-	
+  });
+  
+
+  // Skill animation
+  let block_scroll = $(".skill");
+  let counter = 0;
+
+  $(window).scroll(function() {
+
+    let scroll = $(window).scrollTop() + $(window).height();
+    let offset = block_scroll.offset().top + 200;
+   
+    if (scroll > offset && counter == 0) {
+      $(".skill__line").each(function (){
+        let skill = $(this).children(".skill__count").text();
+        $(this).animate({width: skill+"%"}, 1200);
+      });
+      counter = 1;
+    }
+  });
+  
+  
 });
 
 /* Javascript */
